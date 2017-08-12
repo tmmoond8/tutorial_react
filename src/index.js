be61c5845bec045148d9de00fe0e3bd0c036eb56
Taemin Moon <tmmoond8@gmail.com>
2017-08-12 22:11:24 +0900

    state 사용예제

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import StateExample from './components/StateExample';
 
 const rootElement = document.getElementById('root');
 
-let data = {
-    title: 'hihi',
-    name: 'kkeke',
-    age: 23
-}
-ReactDOM.render(<App data={data}/>, rootElement);
+ReactDOM.render(<StateExample/>, rootElement);
